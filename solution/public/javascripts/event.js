982e2afff074a120b5a0abfa47bfd968a2d938b1
Xuanyi Zhou <Xzhou46@sheffield.ac.uk>
2019-04-05 17:57:23 +0100

    inital commit

---
+++ solution/public/javascripts/event.js
@@ -0,0 +1,46 @@
+<div class="row" >
+    <div class="col-lg-4" style="float: left">
+    <p class="story-information">Story Title</p>
+</div>
+</div>
+<div class="row" >
+    <div class="col-lg-5" style="float: left">
+    <p class="story-information">Story Author</p>
+</div>
+<div class="col-lg-4" style="float: left">
+    <p class="story-information">Story Date</p>
+</div>
+</div>
+<div class="row" style="height: 100px">
+    <div class="col-lg-4" style="float: left;">
+    <p class="story-information" style="font-size: 1.5rem">Story Description</p>
+</div>
+</div>
+<div class="row" style="width: 80%; margin-left: 10%;">
+    <div id="myCarousel1" class="carousel slide" style="margin-top: 0;">
+    <!-- 轮播（Carousel）指标 -->
+<ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <!-- 轮播（Carousel）项目 -->
+    <!--故事图片-->
+    <div class="carousel-inner">
+    <div class="item active">
+    <img class="img-broad" src="/images/broad/broad2.jpeg" alt="First slide">
+    </div>
+    <div class="item">
+    <img class="img-broad" src="/images/broad/broad3.jpg" alt="Second slide">
+    </div>
+    <div class="item">
+    <img class="img-broad" src="/images/broad/broad5.jpg" alt="Third slide">
+    </div>
+    </div>
+    <!-- 轮播（Carousel）导航 -->
+    <a class="carousel-control left" href="#myCarousel1"
+data-slide="prev"> <span _ngcontent-c3="" aria-hidden="true" class="glyphicon glyphicon-chevron-right"></span></a>
+<a class="carousel-control right" href="#myCarousel1"
+data-slide="next">&rsaquo;</a>
+</div>
+</div>
---
+++ solution/routes/index.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+var multer = require("multer");
+var path = require("path");
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './public/uploadpictures');
+    },
+    filename: function(req, file, cb) {
+        var str = file.originalname.split('.');
+        cb(null, Date.now()+str[0]+'.'+str[1]);
+    }
+})
+
+var upload = multer({ storage: storage });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '2222', login_is_correct: true });
+});
+router.get('/index', function(req, res, next) {
+    res.render('index', { title: '2222', login_is_correct: true });
+});
+
+/* GET home page. */
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: '44444', login_is_correct: true });
+});
+
+/* POST from form. */
+router.post('/event', function(req, res, next) {
+    var login_name= req.body.login_name;
+    var login_password= req.body.login_password;
+
+    if (login_name=='qqq' && login_password=='123'){
+        res.render('event', { title: login_name,  login_is_correct: true });
+    } else {
+        res.render('index', { title: 'My Class', login_is_correct: false });
+    }
+});
+
+router.get('/event', function(req, res, next) {
+    var login_name= req.body.login_name;
+    res.render('event', { title: 'login_name', login_is_correct: true });
+});
+router.get('/story', function(req, res, next) {
+    res.render('story', { title: 'hahahah', login_is_correct: true });
+});
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'hahahah', login_is_correct: true });
+});
+router.get('/createevent', function(req, res, next) {
+    var login_name= req.body.login_name;
+    res.render('createevent', { title: 'login_name', login_is_correct: true });
+});
+router.get('/createstory', function(req, res, next) {
+    var login_name= req.body.login_name;
+    res.render('createstory', { title: 'login_name', login_is_correct: true });
+});
+
+router.post('/uploadimg', upload.array("file",9),function(req,res,next){
+    //alert("arrive post!");
+    var arr = [];
+    for(var i in req.files){
+        arr.push(req.files[i].path);
+    }
+    res.send({
+        code:200,
+        data:arr
+    });
+
+});
+
+/* POST from form. */
+// router.post('/event', function(req, res, next) {
+//
+//     res.render('signup', { title: 'My Class', login_is_correct: false });
+//
+// });
+// router.post('/event', function(req, res, next) {
+//
+//     res.render('story', { title: 'My Class', login_is_correct: false });
+//
+// });
+//
+//
+// router.post('/story', function(req, res, next) {
+//
+//     res.render('signup', { title: 'My Class', login_is_correct: false });
+//
+// });
+
+
+module.exports = router;
